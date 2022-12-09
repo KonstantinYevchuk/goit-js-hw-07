2bc17fa8682d28b8df6092dfc7720c96be7b2e23
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-12-09 20:33:18 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery")
+console.log(galleryContainer)
+
+        const murkup = galleryItems.reduce(
+            (acc, {preview, original, description}) => {
+               return acc + 
+               `<div class="gallery__item">
+               <a class="gallery__link" href="${original}">
+                 <img
+                   class="gallery__image"
+                   src="${preview}"
+                   data-source="${original}"
+                   alt="${description}"
+                 />
+               </a>
+             </div>`
+            }, '');
+            
+            galleryContainer.insertAdjacentHTML('beforeend', murkup) 
+   
+    
+galleryContainer.addEventListener("click", onClick);
+const link = document.querySelector(".gallery__link")
+function onClick(evt) {
+    console.log(evt.target);
+    if(evt.target === link) {
+        return
+    } 
+}
+
+
+
+  
