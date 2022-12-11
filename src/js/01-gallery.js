4343fadd4a5633cc7ca1384161a71861f4c27d59
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-12-11 18:51:23 +0200

    Update Task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,32 +5,71 @@ import { galleryItems } from './gallery-items.js';
 const galleryContainer = document.querySelector(".gallery")
 console.log(galleryContainer)
 
-        const murkup = galleryItems.reduce(
-            (acc, {preview, original, description}) => {
-               return acc + 
-               `<div class="gallery__item">
-               <a class="gallery__link" href="${original}">
-                 <img
-                   class="gallery__image"
-                   src="${preview}"
-                   data-source="${original}"
-                   alt="${description}"
-                 />
-               </a>
-             </div>`
-            }, '');
-            
-            galleryContainer.insertAdjacentHTML('beforeend', murkup) 
-   
-    
+// const markup = galleryArray(galleryItems)        
+// galleryContainer.insertAdjacentHTML('beforeend', markup)
+
+// function galleryArray(items) {
+//   const array = items.reduce(
+//     (acc, {preview, original, description}) => {
+//        return acc + 
+//        `<div class="gallery__item">
+//        <a class="gallery__link" href="${original}">
+//          <img
+//            class="gallery__image"
+//            src="${preview}"
+//            data-source="${original}"
+//            alt="${description}"
+//          />
+//        </a>
+//      </div>`
+//     }, '');
+//     return array
+// }
+ 
+const array = galleryItems.reduce(
+      (acc, {preview, original, description}) => {
+         return acc + 
+         `<div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+           <img
+             class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"
+           />
+         </a>
+       </div>`
+      }, '');
+  galleryContainer.insertAdjacentHTML('beforeend', array)
+
 galleryContainer.addEventListener("click", onClick);
-const link = document.querySelector(".gallery__link")
+// const link = document.querySelectorAll(".gallery__link")
+const image = document.querySelectorAll(".gallery__image")
+
 function onClick(evt) {
-    console.log(evt.target);
-    if(evt.target === link) {
-        return
-    } 
+    
+    evt.preventDefault()
+    if(!evt.target.classList.contains("gallery__image")) {
+      return
+    } else {
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+  instance.show()
 }
+}
+
+const body = document.querySelector("body")
+galleryContainer.addEventListener("keydown", evt => {
+  console.log(evt)
+  if(evt.code === "Escape") {
+    console.log("Hello")
+    const instance = basicLightbox.create(``)
+    instance.close()
+  }
+})
+
+
 
 
 
